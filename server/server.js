a131f8b740c7c2cd1cd95449725785c05699adcc
ramonlreyes <ramon.lorareyes@outlook.com>
2025-03-21 17:29:05 +0100

    conected to mongodb ProMern Exercise

--- server/server.js
+++ server/server.js
@@ -7,3 +7,21 @@ app.use(express.static('public'));
 app.listen(3000, function(){
   console.log(`App started on port 3000`); 
 });
+
+
+
+{
+  id:123456,
+  certificate_number:444444,
+  business_name:"IBAW",
+  date:"2021-09-01T22:00:00.000+00:00",
+  result:"Pass",
+  sector:"MongoDB",
+  address:{
+  city:"LUCERNE",
+  zip:6000,
+  street:"Schweizerhofquai",
+  number:1
+  }
+  }
+  
